64bc32f867ffba2da93a4a2b180d0e0c0b6436f6
huigbiuv <108453186+huigbiuv@users.noreply.github.com>
2022-06-30 18:39:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import numpy as np
-
 import streamlit as st
-import pandas as pd
-import numpy as np
 import plotly.express as px
 #from wordcloud import WordCloud, STOPWORDS
-
 import matplotlib.pyplot as plt
 
 st.title("Inventory product analysis")
