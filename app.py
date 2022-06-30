2ca92c932eb28913d27ed16e7dbcbb1eb41f630a
huigbiuv <108453186+huigbiuv@users.noreply.github.com>
2022-06-30 17:25:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,12 @@ st.sidebar.title("Sentiment Analysis of Tweets about US Airlines")
 
 st.markdown(" This application is a Streamlit app used to analyze the sentiment of the tweets 🐦 about US airlines ✈️ ")
 st.sidebar.markdown(" This application is a Streamlit app used to analyze the sentiment of the tweets 🐦 about US airlines ✈️ ")
+
 DATA_URL = ("https://docs.google.com/spreadsheets/d/e/2PACX-1vRffwlmhiARpcl03gaG07TDMbFCbjJOdFpOkscij4-CPIwzNCiQdwC8TsxBjKjN6zV3BUraUDjxUC0p/pub?gid=1245683872&single=true&output=csv")
-
-
 @st.cache(persist=True)
 def load_data():
     data = pd.read_csv(DATA_URL)
-    data['Variant Updated At'] = pd.to_datetime(data['Variant Updated At'])
+    #data['Variant Updated At'] = pd.to_datetime(data['Variant Updated At'])
     return data
 
 data = load_data()
