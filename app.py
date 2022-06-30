76a57a54aa4604a2fd964c3d0baea97f20404efc
huigbiuv <108453186+huigbiuv@users.noreply.github.com>
2022-06-30 18:12:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 #import plotly.express as px
 #from wordcloud import WordCloud, STOPWORDS
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 st.title("Inventory product analysis")
 st.sidebar.title("Inventory product analysis")
