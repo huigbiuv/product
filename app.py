ab7f2b2e4a8c54b73f846073e6ecea95e46d55b6
huigbiuv <108453186+huigbiuv@users.noreply.github.com>
2022-06-30 15:40:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 #import plotly.express as px
 #from wordcloud import WordCloud, STOPWORDS
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 st.title("Sentiment Analysis of Tweets about US Airlines")
 st.sidebar.title("Sentiment Analysis of Tweets about US Airlines")
