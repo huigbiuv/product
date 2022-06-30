d643a60b19ef686606833dc98c0eaa77ab5f4ff7
huigbiuv <108453186+huigbiuv@users.noreply.github.com>
2022-06-30 18:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-#import plotly.express as px
+import plotly.express as px
 #from wordcloud import WordCloud, STOPWORDS
 #import matplotlib.pyplot as plt
 import matplotlib
