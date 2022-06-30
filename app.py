324edc1781c69a2bde23527a2de5f1ed76ad5d98
huigbiuv <108453186+huigbiuv@users.noreply.github.com>
2022-06-30 13:57:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
-
+'''
 num = int(input())
 if num%2 == 0:
     print("It's an Even number!")
 else:
     print("It's an Odd number!")
 
+'''
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
 
+st.title("Sentiment Analysis of Tweets about US Airlines")
+st.sidebar.title("Sentiment Analysis of Tweets about US Airlines")
 
+st.markdown(" This application is a Streamlit app used to analyze the sentiment of the tweets 🐦 about US airlines ✈️ ")
+st.sidebar.markdown(" This application is a Streamlit app used to analyze the sentiment of the tweets 🐦 about US airlines ✈️ ")
